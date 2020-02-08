29b1d04d10c7e4a1b58d3a1b88bb309a1e9f05bc
cj <cj@silentorb.com>
2020-02-08 09:19:54 -0700

    Moved the Imp IntelliJ plugin into its own repo.

--- projects/imp_intellij/src/main/kotlin/silentorb.imp.intellij/ImpIcon.kt
+++
@@ -1,7 +0,0 @@
-package silentorb.imp.intellij
-
-import com.intellij.openapi.util.IconLoader
-
-object ImpIcons {
-  val FILE = IconLoader.getIcon("/icons/familiar.png")
-}
--- projects/imp_intellij/src/main/kotlin/silentorb/imp/intellij/language/ImpLanguage.kt
+++
@@ -1,9 +0,0 @@
-package silentorb.imp.intellij.language
-
-import com.intellij.lang.Language
-
-class ImpLanguage() : Language("Imp") {
-  companion object {
-    val INSTANCE = ImpLanguage()
-  }
-}
